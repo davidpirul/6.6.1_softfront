1f8f773baf64b0e3df7980cdd91b7997d1d724ce
David Pirul <david.epa@live.com>
2023-02-07 00:47:53 -0300

    Update Login.jsx

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -16,7 +16,7 @@ export default function RegistroForm() {
   };
 
   const iniciarSesion = async () => {
-    const urlServer = "process.env.VITE_BACKEND_URL";
+    const urlServer = "https://6-6-1-softfront.vercel.app/";
     const endpoint = "/login";
     const { email, password } = usuario;
     try {
